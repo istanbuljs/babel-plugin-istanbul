f870a8f67b2d04f156fd1b91bc1faab54915ef71
Benjamin E. Coe <bencoe@gmail.com>
2017-01-04 02:29:37 -0800

    fix: address regression related to export const foo = () => {} (#79)

---
+++ fixtures/issue-78.js
@@ -0,0 +1,12 @@
+export const updateName = (name) => {
+  return {
+    type: 'UPDATE_NAME',
+    name
+  }
+}
+export const updateName2 = (name) => {
+  return {
+    type: 'UPDATE_NAME',
+    name
+  }
+}
