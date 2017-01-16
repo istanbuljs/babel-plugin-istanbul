c352dd3efe9c18d4b6fad47ef782347503d1d56b
alpersogukpinar <asogukpinar@gmail.com>
2017-01-16 15:49:17 +0200

    Update index.js
    nyc command line include parameter was ignored and package.json nyc/include values were used everytime. Fixed that problem.
    Fix #82

--- src/index.js
+++ src/index.js
@@ -19,9 +19,11 @@ function makeShouldSkip () {
   return function shouldSkip (file, opts) {
     if (!exclude) {
       const cwd = getRealpath(process.env.NYC_CWD || process.cwd())
+      const config = JSON.parse(process.env.NYC_CONFIG)
       exclude = testExclude(assign(
         { cwd },
         Object.keys(opts).length > 0 ? opts : {
+          include: config.include,
           configKey: 'nyc',
           configPath: dirname(findUp.sync('package.json', { cwd }))
         }
