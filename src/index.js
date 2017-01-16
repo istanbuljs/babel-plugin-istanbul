e55b6dc3736261e151a040d1ffd0e5bafc4e0ce3
alpersogukpinar <asogukpinar@gmail.com>
2017-01-16 23:30:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,11 +19,11 @@ function makeShouldSkip () {
   return function shouldSkip (file, opts) {
     if (!exclude) {
       const cwd = getRealpath(process.env.NYC_CWD || process.cwd())
-      const include = process.env.NYC_CONFIG ? JSON.parse(process.env.NYC_CONFIG).include : false
+      const defaults = process.env.NYC_CONFIG ? JSON.parse(process.env.NYC_CONFIG) : {}
       exclude = testExclude(assign(
         { cwd },
         Object.keys(opts).length > 0 ? opts : {
-          include: include,
+          include: defaults.include,
           configKey: 'nyc',
           configPath: dirname(findUp.sync('package.json', { cwd }))
         }
