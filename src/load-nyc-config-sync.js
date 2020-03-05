e5d2f7e662776d430ccef45279801cd182893caf
Sylvester Keil <sylvester@keil.or.at>
2020-03-05 17:49:46 +0100

    Remove isElectron check

--- src/load-nyc-config-sync.js
+++ src/load-nyc-config-sync.js
@@ -2,13 +2,12 @@
 'use strict'
 
 const { loadNycConfig } = require('@istanbuljs/load-nyc-config')
-const isElectron = process.versions && process.versions.electron
 
 async function main () {
   const [cwd, nycrcPath] = process.argv.slice(2)
 
   console.log(JSON.stringify(await loadNycConfig({ cwd, nycrcPath })))
-  if (isElectron) process.exit(0)
+  process.exit(0)
 }
 
 main().catch(error => {
