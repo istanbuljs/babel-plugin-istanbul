fb1d78c5c7b494511b0666f1a9ed14d73a57d818
Dolan Murvihill <dmurvihill@gmail.com>
2025-09-24 05:46:48 -0700

    feat(deps)!: update to latest version of test-exclude (#301)
    Co-authored-by: Simen Bekkhus <sbekkhus91@gmail.com>

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,9 @@ module.exports = {
   plugins: [
     '@babel/plugin-transform-modules-commonjs'
   ],
+  ignore: [
+    '**/test/babel-8/node_modules/**'
+  ],
   env: {
     test: {
       plugins: ['./lib']
